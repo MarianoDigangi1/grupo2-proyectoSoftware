974bba4be291172a2e834a883115054ad760c2b6
MarianoDigangi1 <marianodigangi1@gmail.com>
2020-09-18 00:12:31 -0300

    paquete controller y carpetas

--- src/main/java/com/proyecto/software/grupo2proyectoSoftware/repositories/PersonaRepositories.java
+++ src/main/java/com/proyecto/software/grupo2proyectoSoftware/repositories/PersonaRepositories.java
@@ -1,10 +1,7 @@
 package com.proyecto.software.grupo2proyectoSoftware.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.proyecto.software.grupo2proyectoSoftware.entities.Persona;
-
-
 
 public interface PersonaRepositories  extends JpaRepository <Persona, Integer>{
 
