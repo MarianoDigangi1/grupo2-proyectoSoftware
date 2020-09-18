686aed9f228437cbb55ec2df4a2d0f79e2909543
carolinagomez.gc@gmail.com <carolinagomez.gc@gmail.com>
2020-09-17 21:44:29 -0300

    Creacion de paquetes

---
+++ src/main/java/com/proyecto/software/grupo2proyectoSoftware/entities/Domicilio.java
@@ -0,0 +1,94 @@
+package com.proyecto.software.grupo2proyectoSoftware.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Domicilio {
+ private int idDomicilio;
+ private String calle;
+ private int numero;
+ private String ciudad;
+ private String provincia;
+ private int codigoPostal;
+ 
+ 
+ public Domicilio() {
+	 
+ }
+
+
+public Domicilio(int idDomicilio, String calle, int numero, String ciudad, String provincia, int codigoPostal) {
+	super();
+	this.idDomicilio = idDomicilio;
+	this.calle = calle;
+	this.numero = numero;
+	this.ciudad = ciudad;
+	this.provincia = provincia;
+	this.codigoPostal = codigoPostal;
+}
+
+
+public int getIdDomicilio() {
+	return idDomicilio;
+}
+
+
+public void setIdDomicilio(int idDomicilio) {
+	this.idDomicilio = idDomicilio;
+}
+
+
+public String getCalle() {
+	return calle;
+}
+
+
+public void setCalle(String calle) {
+	this.calle = calle;
+}
+
+
+public int getNumero() {
+	return numero;
+}
+
+
+public void setNumero(int numero) {
+	this.numero = numero;
+}
+
+
+public String getCiudad() {
+	return ciudad;
+}
+
+
+public void setCiudad(String ciudad) {
+	this.ciudad = ciudad;
+}
+
+
+public String getProvincia() {
+	return provincia;
+}
+
+
+public void setProvincia(String provincia) {
+	this.provincia = provincia;
+}
+
+
+public int getCodigoPostal() {
+	return codigoPostal;
+}
+
+
+public void setCodigoPostal(int codigoPostal) {
+	this.codigoPostal = codigoPostal;
+}
+ 
+ 
+ 
+}
+ 
+ 
