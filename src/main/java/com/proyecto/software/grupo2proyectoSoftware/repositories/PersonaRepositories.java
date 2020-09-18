b878fad31bc840c5048dc66953b59c7a6a719bbb
carolinagomez.gc@gmail.com <carolinagomez.gc@gmail.com>
2020-09-17 22:57:43 -0300

    prueba

---
+++ src/main/java/com/proyecto/software/grupo2proyectoSoftware/repositories/PersonaRepositories.java
@@ -0,0 +1,13 @@
+package com.proyecto.software.grupo2proyectoSoftware.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.software.grupo2proyectoSoftware.entities.Persona;
+
+
+
+public interface PersonaRepositories  extends JpaRepository <Persona, Integer>{
+
+	
+
+}
